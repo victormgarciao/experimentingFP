9e71187bcbff9731c717fd08ac5bb3d92a247ab4
Victor Garcia <vimago81@gmail.com>
2020-05-05 00:07:19 +0200

    refactor flowAsync util

--- src/utils/fp.utils.js
+++ src/utils/fp.utils.js
@@ -1,19 +1,12 @@
-import { isEmpty, drop, first } from 'lodash';
-
-export const removeFirstItemFrom = drop;
-export const selectFirstItemFrom = first;
+import { isEmpty } from 'lodash';
 
 
 export function flowAsync(...functionsList) {
     
     return async function handleResult(...args) {
-        const newFunctionsList = functionsList;
-        const currentFunction = selectFirstItemFrom(newFunctionsList);
+        const [ currentFunction, ...nextFunctionsList] = functionsList;
 
         const currentResult = await Promise.resolve(currentFunction(...args));
-        
-
-        const nextFunctionsList = removeFirstItemFrom(newFunctionsList);
 
         return isEmpty(nextFunctionsList)
             ? currentResult
